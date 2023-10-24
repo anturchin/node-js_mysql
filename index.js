aeba4e1beb81be4d777624905cb2ebaa7dec253a
andrey_turchin <tur4intwo@gmail.com>
2023-10-24 17:07:44 +0300

    add cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(cors())
 
 app.use((req, res) => {
 	res.send('ok');
